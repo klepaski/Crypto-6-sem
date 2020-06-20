6d2d8ae15f713bf15a023a0be257c2276c517978
Klepaski <43060010+klepaski@users.noreply.github.com>
2020-06-20 13:50:26 +0300

    Add files via upload

---
+++ 1.2 - Энтропия/KriptografyLab2/Alphabet.cs
@@ -0,0 +1,133 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XL = Microsoft.Office.Interop.Excel;
+
+namespace KriptografyLab2
+{
+    class Alphabet
+    {
+        int[] colFx;
+        double[] verSim;
+
+        char[] korean = { 'ㅏ','ㅑ', 'ㅓ','ㅕ','ㅗ','ㅛ','ㅜ','ㅠ','ㅣ','ㅡ','ㄱ','ㄴ','ㄷ','ㄹ','ㅁ',
+                          'ㅂ','ㅅ','ㅇ','ㅈ','ㅎ','ㅋ','ㅌ','ㅍ','ㅊ','ㄲ','ㄸ','ㅃ','ㅆ','ㅉ',
+                          'ㅐ','ㅔ','ㅒ','ㅖ','ㅚ','ㅟ','ㅘ','ㅝ','ㅙ','ㅞ','ㅢ'};
+
+        char[] cyrillic = {'А','Б','В','Г','Д','Е','Ё','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч',
+                           'Ш','Щ','Ъ','Ы','Ь','Э','Ю','Я'};
+        char[] latin = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'V', 'X', 'Y', 'Z' };
+        char[] binary = {'0','1'};
+        
+        public char[] Korean { get => korean; set => korean = value; }
+        public char[] Cyrillic { get => cyrillic; set => cyrillic = value; }
+        public char[] Latin { get => latin; set => latin = value; }
+        public char[] Binary { get => binary; set => binary = value; }
+
+
+
+        public double Entropy(String file,char [] alf)
+        {
+            colFx = new int[alf.Length]; // абвгд - 0023005
+
+            using (StreamWriter sw = new StreamWriter("otvet.txt", true, System.Text.Encoding.Unicode))
+            {
+                if (alf == Korean) sw.WriteLine("\nКоличество вхождений символа:");
+                for (int i = 0; i < alf.Length; i++)                                                        ///каждая буква алфавита провер. кол-во вхожд во фразе
+                {
+                    colFx[i] = file.ToUpper().Where(el => el == alf[i]).Count();                            ///кол-во вхожд буквы во фразе
+                    if (colFx[i] != 0)
+                        sw.WriteLine(colFx[i] + " " + alf[i]);                                              ///есть вхожд - вывод букву + кол-во
+                }
+
+
+
+                if (alf==Korean) sw.WriteLine("Вероятность появления символа:");
+                verSim = new double[alf.Length];
+                for (int i = 0; i < alf.Length; i++)
+                {
+                    if (colFx[i] != 0)
+                    {
+                        verSim[i] = Math.Round((double)colFx[i] / file.Length, 2);  ///кол-во вхожд / длина стр
+                        sw.WriteLine(verSim[i] + " " + alf[i]);
+                    }
+                }
+                double res = 0;
+                for (int i = 0; i < alf.Length; i++)
+                {
+                    if (verSim[i] != 0)
+                        res += verSim[i] * Math.Log(verSim[i], 2);
+                }
+                return (-res);
+            }
+        }
+
+
+
+        public double kolInf(double ResEntr,int kolSimv)
+        {
+            return ResEntr * (double)kolSimv;
+        }
+
+
+
+        public string GetBytes(String str)
+        {
+            String strB = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                strB += Convert.ToString( (int)str[i], 2 ) + " ";
+            }
+            return strB;
+        }
+
+
+        public double EfectEntropy(double verError)
+        {
+            /// He(A) = 1 - H(Y|X)
+            /// H(Y|X) = - p log2(p) - q log2(q)
+            return 1 - (-verError * Math.Log(verError, 2) - (1 - verError) * Math.Log((1 - verError), 2));
+        }
+
+
+       
+        public void BuildGistogram(char[] alf)
+        {
+            XL.Application XL1 = new XL.Application();  ///объяв объект
+            XL1.Workbooks.Add();                         ///доб. рабочую книгу
+
+            XL1.ActiveSheet.Range["A1"].Value = "Символ:";
+            XL1.ActiveSheet.Range["B1"].Value = "Частота:";
+
+
+            int Yach = 2;
+            for (int i = 0; i < colFx.Length; i++)
+            {
+                if (colFx[i] != 0)
+                {
+                    XL1.ActiveSheet.Range["A" + Yach].Value = $"{alf[i]}";
+                    XL1.ActiveSheet.Range["B" + Yach].Value = Convert.ToInt32(colFx[i]);
+                    Yach++;
+                }
+            }
+
+            XL1.Charts.Add();   ///доб. новую диаграмму
+            XL1.ActiveChart.ChartType = XL.XlChartType.xlColumnClustered;///столбчатая
+
+            //подписи по оси X
+            XL1.ActiveChart.Axes(XL.XlAxisType.xlCategory).HasTitle = true;
+            XL1.ActiveChart.Axes(XL.XlAxisType.xlCategory).AxisTitle.Characters.Text = "Частота появления";
+
+            //подписи по оси Y
+            XL1.ActiveChart.Axes(XL.XlAxisType.xlCategory).HasTitle = true;
+            XL1.ActiveChart.Axes(XL.XlAxisType.xlCategory).AxisTitle.Characters.Text = "Символы";
+
+            XL1.ActiveChart.Export(@"D:\myFile.jpg");
+            XL1.Visible = true;
+        }
+    }
+}
---
+++ 1.2 - Энтропия/KriptografyLab2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KriptografyLab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //949 — Korean, 1253 — Greek,   65001 (UTF-8)
+            //Console.WriteLine("안녕하세요");
+
+            Console.OutputEncoding = Encoding.Unicode;
+
+            String text="";
+            Alphabet alphabet = new Alphabet();
+            using (StreamReader sr = new StreamReader("korean.txt"))
+            {
+                text=(sr.ReadToEnd());
+                text = text.Replace(" ","");
+            }
+
+            using (StreamWriter sw = new StreamWriter("otvet.txt", false, System.Text.Encoding.Unicode))
+            {
+                sw.WriteLine(text);
+            }
+
+
+            double EntKor = alphabet.Entropy(text, alphabet.Korean);
+            alphabet.BuildGistogram(alphabet.Korean);
+
+            double EntBin = alphabet.Entropy(text, alphabet.Binary);
+
+            using (StreamWriter sw = new StreamWriter("otvet.txt", true, System.Text.Encoding.Unicode))
+            {
+                sw.WriteLine();
+                sw.WriteLine("Entropy korean: " + EntKor);
+                ///sw.WriteLine("Entropy cyrillic: " + alphabet.Entropy(text, alphabet.Cyrillic));
+                ///sw.WriteLine("Entropy latin: " + alphabet.Entropy(text, alphabet.Latin));
+                sw.WriteLine("Entropy binary: " + EntBin);
+                sw.WriteLine("Infor korean: " + alphabet.kolInf(EntKor, text.Length));
+            }
+            
+
+
+            using (StreamWriter sw = new StreamWriter("byt.txt", false, System.Text.Encoding.Default))
+            {
+                sw.WriteLine(alphabet.GetBytes("ㅊㅣㅅㅌㅑㄱㅓㅘ ㅠㄹㅣㅑ"));
+            }
+            
+            using (StreamReader sr = new StreamReader("byt.txt"))
+            {
+                text = (sr.ReadToEnd());
+                text = text.Replace(" ", "");
+            }
+            
+
+            using (StreamWriter sw = new StreamWriter("Infor.txt", false, System.Text.Encoding.Unicode))
+            {
+                sw.WriteLine("Infor binary: " + alphabet.kolInf(alphabet.Entropy(text, alphabet.Binary), text.Length));
+                sw.WriteLine("Effect Entropy (ERROR 0.1) : " + alphabet.EfectEntropy(0.1));
+                sw.WriteLine("Effect Entropy (ERROR 0.5) : " + alphabet.EfectEntropy(0.5));
+                sw.WriteLine("Effect Entropy (ERROR 1)   : " + alphabet.EfectEntropy(1));
+                ///P ошибочн передачи единич бита
+            }
+
+            Console.WriteLine("Выполнено успешно!");
+            Console.ReadKey();
+        }
+    }
+}
